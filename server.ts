8399da2ef7c8a0cdbf5c01a4b63ed499ca5f804a
Raven Jiang <raven@cs.stanford.edu>
2020-05-23 19:04:47 -0700

    Basic synchronization

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import eetase from 'eetase';
-import express from 'express';
+import express, { request } from 'express';
 import http from 'http';
 import morgan from 'morgan';
 import path from 'path';
@@ -11,24 +11,37 @@ import uuid from 'uuid';
 import * as wireplace from './wireplace';
 
 const ENVIRONMENT = process.env.ENV || 'dev';
-const SOCKETCLUSTER_PORT = (process.env.SOCKETCLUSTER_PORT ? parseInt(process.env.SOCKETCLUSTER_PORT, 10) : undefined) || 8000;
+const SOCKETCLUSTER_PORT =
+  (process.env.SOCKETCLUSTER_PORT
+    ? parseInt(process.env.SOCKETCLUSTER_PORT, 10)
+    : undefined) || 8000;
 const SOCKETCLUSTER_WS_ENGINE = process.env.SOCKETCLUSTER_WS_ENGINE || 'ws';
-const SOCKETCLUSTER_SOCKET_CHANNEL_LIMIT = Number(process.env.SOCKETCLUSTER_SOCKET_CHANNEL_LIMIT) || 1000;
+const SOCKETCLUSTER_SOCKET_CHANNEL_LIMIT =
+  Number(process.env.SOCKETCLUSTER_SOCKET_CHANNEL_LIMIT) || 1000;
 const SOCKETCLUSTER_LOG_LEVEL = process.env.SOCKETCLUSTER_LOG_LEVEL || 2;
 
 const SCC_INSTANCE_ID = uuid.v4();
 const SCC_STATE_SERVER_HOST = process.env.SCC_STATE_SERVER_HOST || undefined;
-const SCC_STATE_SERVER_PORT = process.env.SCC_STATE_SERVER_PORT ? parseInt(process.env.SCC_STATE_SERVER_PORT, 10) : undefined;
+const SCC_STATE_SERVER_PORT = process.env.SCC_STATE_SERVER_PORT
+  ? parseInt(process.env.SCC_STATE_SERVER_PORT, 10)
+  : undefined;
 const SCC_MAPPING_ENGINE = process.env.SCC_MAPPING_ENGINE || undefined;
-const SCC_CLIENT_POOL_SIZE = process.env.SCC_CLIENT_POOL_SIZE ? parseInt(process.env.SCC_CLIENT_POOL_SIZE, 10) : undefined;
+const SCC_CLIENT_POOL_SIZE = process.env.SCC_CLIENT_POOL_SIZE
+  ? parseInt(process.env.SCC_CLIENT_POOL_SIZE, 10)
+  : undefined;
 const SCC_AUTH_KEY = process.env.SCC_AUTH_KEY || undefined;
 const SCC_INSTANCE_IP = process.env.SCC_INSTANCE_IP || undefined;
 const SCC_INSTANCE_IP_FAMILY = process.env.SCC_INSTANCE_IP_FAMILY || undefined;
-const SCC_STATE_SERVER_CONNECT_TIMEOUT = Number(process.env.SCC_STATE_SERVER_CONNECT_TIMEOUT) || undefined;
-const SCC_STATE_SERVER_ACK_TIMEOUT = Number(process.env.SCC_STATE_SERVER_ACK_TIMEOUT) || undefined;
-const SCC_STATE_SERVER_RECONNECT_RANDOMNESS = Number(process.env.SCC_STATE_SERVER_RECONNECT_RANDOMNESS) || undefined;
-const SCC_PUB_SUB_BATCH_DURATION = Number(process.env.SCC_PUB_SUB_BATCH_DURATION) || undefined;
-const SCC_BROKER_RETRY_DELAY = Number(process.env.SCC_BROKER_RETRY_DELAY) || undefined;
+const SCC_STATE_SERVER_CONNECT_TIMEOUT =
+  Number(process.env.SCC_STATE_SERVER_CONNECT_TIMEOUT) || undefined;
+const SCC_STATE_SERVER_ACK_TIMEOUT =
+  Number(process.env.SCC_STATE_SERVER_ACK_TIMEOUT) || undefined;
+const SCC_STATE_SERVER_RECONNECT_RANDOMNESS =
+  Number(process.env.SCC_STATE_SERVER_RECONNECT_RANDOMNESS) || undefined;
+const SCC_PUB_SUB_BATCH_DURATION =
+  Number(process.env.SCC_PUB_SUB_BATCH_DURATION) || undefined;
+const SCC_BROKER_RETRY_DELAY =
+  Number(process.env.SCC_BROKER_RETRY_DELAY) || undefined;
 
 let agOptions = {};
 
@@ -62,23 +75,54 @@ expressApp.get('/health-check', (req, res) => {
 
 // SocketCluster/WebSocket connection handling loop.
 (async () => {
-  for await (let {socket} of agServer.listener('connection')) {
+  let intervalId: NodeJS.Timeout | undefined = undefined;
+
+  for await (let { socket } of agServer.listener('connection')) {
     // Handle socket connection.
     console.log('Connected', socket.id);
 
     (async () => {
       for await (let request of socket.procedure('join')) {
-        const actorId = wireplace.join();
+        const actorId = wireplace.join(socket.id);
         console.log('New actor', actorId);
         request.end(actorId);
       }
     })();
 
     (async () => {
-      for await (let data of socket.receiver('move')) {
-        wireplace.move(data);
+      for await (let request of socket.procedure('sync')) {
+        const diff = wireplace.sync(socket.id);
+        request.end(diff);
       }
     })();
+
+    (async () => {
+      for await (let request of socket.procedure('move')) {
+        wireplace.move(request.data);
+        request.end(true);
+      }
+    })();
+
+    intervalId = setInterval(() => {
+      const diff = wireplace.getUpdate();
+      if (!diff) {
+        return;
+      }
+      socket.exchange.transmitPublish('update', diff);
+    }, 10);
+  }
+
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+  console.log('Main loop terminated');
+})();
+
+(async () => {
+  for await (let { socket } of agServer.listener('disconnection')) {
+    // Handle socket connection.
+    console.log('Disconnected', socket.id);
+    wireplace.leave(socket.id);
   }
 })();
 
@@ -86,7 +130,7 @@ httpServer.listen(SOCKETCLUSTER_PORT);
 
 if (SOCKETCLUSTER_LOG_LEVEL >= 1) {
   (async () => {
-    for await (let {error} of agServer.listener('error')) {
+    for await (let { error } of agServer.listener('error')) {
       console.error(error);
     }
   })();
@@ -94,11 +138,13 @@ if (SOCKETCLUSTER_LOG_LEVEL >= 1) {
 
 if (SOCKETCLUSTER_LOG_LEVEL >= 2) {
   console.log(
-    `   ${colorText('[Active]', 32)} SocketCluster worker with PID ${process.pid} is listening on port ${SOCKETCLUSTER_PORT}`
+    `   ${colorText('[Active]', 32)} SocketCluster worker with PID ${
+      process.pid
+    } is listening on port ${SOCKETCLUSTER_PORT}`
   );
 
   (async () => {
-    for await (let {warning} of agServer.listener('warning')) {
+    for await (let { warning } of agServer.listener('warning')) {
       console.warn(warning);
     }
   })();
@@ -121,18 +167,18 @@ if (SCC_STATE_SERVER_HOST) {
     pubSubBatchDuration: SCC_PUB_SUB_BATCH_DURATION,
     stateServerHost: SCC_STATE_SERVER_HOST,
     stateServerPort: SCC_STATE_SERVER_PORT,
-    mappingEngine: <any> SCC_MAPPING_ENGINE,
+    mappingEngine: <any>SCC_MAPPING_ENGINE,
     clientPoolSize: SCC_CLIENT_POOL_SIZE,
     authKey: SCC_AUTH_KEY,
     stateServerConnectTimeout: SCC_STATE_SERVER_CONNECT_TIMEOUT,
     stateServerAckTimeout: SCC_STATE_SERVER_ACK_TIMEOUT,
     stateServerReconnectRandomness: SCC_STATE_SERVER_RECONNECT_RANDOMNESS,
-    brokerRetryDelay: SCC_BROKER_RETRY_DELAY
+    brokerRetryDelay: SCC_BROKER_RETRY_DELAY,
   });
 
   if (SOCKETCLUSTER_LOG_LEVEL >= 1) {
     (async () => {
-      for await (let {error} of sccClient.listener('error')) {
+      for await (let { error } of sccClient.listener('error')) {
         error.name = 'SCCError';
         console.error(error);
       }
--- wireplace.ts
+++ wireplace.ts
@@ -1,13 +1,19 @@
 import { WirePlaceScene } from 'wireplace-scene';
+import type { Diff } from 'wireplace-scene';
 
 let globalId = 0;
 
+type UserID = string;
+type ActorID = string;
+
 type Room = {
   scene: WirePlaceScene;
-}
+};
+
+const users: Record<UserID, ActorID> = {};
 
 const rooms: Record<string, Room> = {
-  'default': {
+  default: {
     scene: new WirePlaceScene(),
   },
 };
@@ -16,19 +22,35 @@ function getRoom(roomId = 'default') {
   return rooms[roomId];
 }
 
-function join() {
-  const actorId = globalId.toString();
+function join(userId: UserID): ActorID {
+  const actorId = `a${globalId}`;
   globalId += 1;
   const { scene } = getRoom();
   scene.addActor(actorId);
+  users[userId] = actorId;
   return actorId;
+}
+
+function leave(userId: UserID): boolean {
+  const actorId = users[userId];
+  const { scene } = getRoom();
+  return scene.removeActor(actorId);
+}
+
+function sync(userId: UserID): string {
+  const { scene } = getRoom();
+  const { data } = scene.retrieveSerializedDiff(true);
+  return data;
+}
+
+function getUpdate(): string | null {
+  const { scene } = getRoom();
+  const { count, data } = scene.retrieveSerializedDiff(false);
+  return count > 0 ? data : null;
 }
 
 function move(data: string) {
   console.log('move', data);
 }
 
-export {
-  move,
-  join,
-};
+export { leave, move, join, sync, getUpdate };
