2c395aeb6942481f26fb02bd9d24602a37fb1d8a
Raven Jiang <raven@cs.stanford.edu>
2020-05-31 20:09:20 -0700

    pm2 ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+// pm2 ecosystem file
+module.exports = {
+  apps : [{
+    name: "wireplace-server",
+    script: "./dist/server.js",
+    env: {
+      NODE_ENV: "development",
+      ENV: "dev",
+      SOCKETCLUSTER_PORT: 8000
+    },
+    env_production: {
+      NODE_ENV: "production",
+      ENV: "prod",
+      SOCKETCLUSTER_PORT: 8080,
+      WIREPLACE_SSL_CERT: "/etc/letsencrypt/live/server.wireplace.net/fullchain.pem",
+      WIREPLACE_SSL_KEY: "/etc/letsencrypt/live/server.wireplace.net/privkey.pem"
+    }
+  }]
+}
