37a2aec9a2ce6fbefbbc21874595f6751c4c2815
Raven Jiang <raven@cs.stanford.edu>
2020-05-31 20:13:08 -0700

    Exponential backoff restart for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
   apps : [{
     name: "wireplace-server",
     script: "./dist/server.js",
+    exp_backoff_restart_delay: 100,
     env: {
       NODE_ENV: "development",
       ENV: "dev",
